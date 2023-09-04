21b81f973b838dbcab7aa4b422e43b109b9d5261
AakashNakarmi <aakasnakarmee@gmail.com>
2023-09-04 15:22:07 +0530

    Initial commit

---
+++ src/main/java/com/example/lms/controller/LMSController.java
@@ -0,0 +1,17 @@
+package com.example.lms;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LMSController {
+
+
+
+
+    @GetMapping("/")
+    public String loginPage(){
+        return "login";
+    }
+}
